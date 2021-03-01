d36499fa41f2739dc9941f0d43aae2ca74d373df
KuhnMatthias <70940866+KuhnMatthias@users.noreply.github.com>
2021-03-01 10:48:16 +0100

    Create Blink_button.py

---
+++ Blink_button.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GIPO     # import RPi.GPOP module (to control the GPIO Pins)
+import time                 # import time module (to use time operations)
+GIPO.setmode(GIPO.BOARD)    # set to the use of board numbering
+GIPO.setup(13,GIPO.OUT)     # set the GIPO pin 13 as an input
+GIPO.setup(15,GIPO.IN)
+while True:
+    if GIPO.input(15) ==0:
+        GIPO.output(13,True)
+        time.sleep(1)
+        GIPO.output(13,False)
+        time.sleep(1)
+    else:
+        GIPO.output(13, True)
