1a5a1e2b4e96458d49507297e67e616116b5ca69
KuhnMatthias <70940866+KuhnMatthias@users.noreply.github.com>
2021-03-01 10:48:21 +0100

    Update Blink.py

--- Blink.py
+++ Blink.py
@@ -2,7 +2,6 @@ import RPi.GPIO as GIPO     # import RPi.GPOP module (to control the GPIO Pins)
 import time                 # import time module (to use time operations)
 GIPO.setmode(GIPO.BOARD)    # set to the use of board numbering
 GIPO.setup(13,GIPO.OUT)     # set the GIPO pin 13 as an input
-while True:
     GIPO.output(13,True)
     time.sleep(1)
     GIPO.output(13,False)
